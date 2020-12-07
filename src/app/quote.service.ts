83e79ebbc08e0326d1bf23a5476aa4f1965b373d
VladGrigorovich <vladyslavhryhorovych@gmail.com>
2020-12-07 15:25:00 +0200

    Add quote service.

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Quote } from './shared/interfaces/quote';
+import { RandomQuoteProps } from './shared/interfaces/random-quote-props';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuoteService {
-  constructor() {}
+  constructor(private readonly httpClient: HttpClient) {}
+
+  public getRandomQuote(props: RandomQuoteProps): Observable<Quote> {
+    let url = `${environment.api.chuck}/jokes/random?`;
+    if (props.name && props.name.length > 0) {
+      url += `name=${name}&`;
+    }
+    if (props.categories && props.categories.length > 0) {
+      const categoriesQuery = props.categories.join(',');
+      url += `categories=${categoriesQuery}&`;
+    }
+    return this.httpClient.get<Quote>(url);
+  }
+
+  public searchQuotes(query: string): Observable<Quote[]> {
+    const url = `${environment.api.chuck}/jokes/search?query=${query}`;
+    return this.httpClient.get<Quote[]>(url);
+  }
 }
---
+++ src/app/shared/interfaces/random-quote-props.ts
@@ -0,0 +1,4 @@
+export interface RandomQuoteProps {
+  name?: string;
+  categories?: string[];
+}
